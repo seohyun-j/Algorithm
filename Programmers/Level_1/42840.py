5c8a94ed2ee101e4bf1fa43a31980fb1f1ca9b88
Seohyun Jang <seohyun_j@kakao.com>
2022-03-23 23:14:19 +0900

    모의고사

--- Programmers/Level_1/42840.py
+++ Programmers/Level_1/42840.py
@@ -1,1 +1,16 @@
 # 모의고사
+a0 = [1, 2, 3, 4, 5]
+a1 = [1, 3, 2, 4, 2]
+
+
+def solution(answers):
+    n0 = [1, 2, 3, 4, 5]
+    n1 = [2, 1, 2, 3, 2, 4, 2, 5]
+    n2 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    answer = []
+
+    return answer
+
+
+print(solution(a0))
+print(solution(a1))
