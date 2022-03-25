937590d27587a48065360cdefa9b5006f1f2c8cb
Seohyun Jang <seohyun_j@kakao.com>
2022-03-25 22:48:55 +0900

    H-Index

---
+++ Programmers/Level_2/42747.py
@@ -0,0 +1,12 @@
+c0 = [3, 0, 6, 1, 5]
+
+
+def solution(citations):
+    citations.sort(reverse=True)
+    for idx, citation in enumerate(citations):
+        if idx >= citation:
+            return idx
+    return len(citations)
+
+
+print(solution(c0))
