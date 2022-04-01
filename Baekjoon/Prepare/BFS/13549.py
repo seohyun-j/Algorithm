1704eedda4d5f7c012390f6245e7774f08590b94
Seohyun Jang <seohyun_j@kakao.com>
2022-04-01 16:07:19 +0900

    숨바꼭질3

--- Baekjoon/Prepare/BFS/13549.py
+++ Baekjoon/Prepare/BFS/13549.py
@@ -2,7 +2,8 @@ from collections import deque
 
 n, k = map(int, input().split())
 MAX = 100000
-dist = [0]*(MAX+1)
+dist = [0] * (MAX + 1)
+
 
 def bfs():
     queue = deque()
@@ -14,13 +15,14 @@ def bfs():
             print(dist[x])
             break
 
-        for j in (x-1, x+1, x*2):
+        for j in (x - 1, x + 1, x * 2):
             if 0 <= j <= MAX and not dist[j]:
-                if j == x*2:
+                if j == x * 2:
                     dist[j] = dist[x]
                 else:
                     dist[j] = dist[x] + 1
                 queue.append(j)
+
 
 bfs()
 
