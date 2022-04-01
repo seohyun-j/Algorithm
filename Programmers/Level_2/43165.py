2328f3cb250a3e1c74a37501af94db5e004bbb0c
Seohyun Jang <seohyun_j@kakao.com>
2022-04-01 16:52:28 +0900

    타겟넘버

--- Programmers/Level_2/43165.py
+++ Programmers/Level_2/43165.py
@@ -4,6 +4,8 @@ t0 = 3
 t1 = 4
 
 from collections import deque
+
+
 def solution(numbers, target):
     answer = 0
     queue = deque()
@@ -21,9 +23,11 @@ def solution(numbers, target):
                 answer += 1
     return answer
 
+
 def d_solution(numbers, target):
     n = len(numbers)
     answer = 0
+
     def dfs(idx, result):
         if idx == n:
             if result == target:
@@ -31,14 +35,14 @@ def d_solution(numbers, target):
                 answer += 1
             return
         else:
-            dfs(idx+1, result+numbers[idx])
-            dfs(idx+1, result-numbers[idx])
+            dfs(idx + 1, result + numbers[idx])
+            dfs(idx + 1, result - numbers[idx])
+
     dfs(0, 0)
     return answer
 
 
 print(solution(n0, t0))
 print(solution(n1, t1))
-
 
 # 이 문제에서는 bfs보다 dfs가 속도와 메모리 측면에서 효율적이다.
