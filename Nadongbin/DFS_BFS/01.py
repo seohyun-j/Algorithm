b72236efbb4845d8e1e89ca566414e8377607707
Seohyun Jang <seohyun_j@kakao.com>
2022-04-18 16:09:58 +0900

    음료수 얼려먹기

---
+++ Nadongbin/DFS_BFS/01.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input())))
+
+answer = 0
+
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if arr[x][y] == 0:
+        arr[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            answer += 1
+
+print(answer)
--- Nadongbin/Greedy/09.py
+++ Nadongbin/Greedy/09.py
@@ -2,4 +2,3 @@ food_times = [3, 1, 2]
 k = 5
 
 for i in range(5):
-    
