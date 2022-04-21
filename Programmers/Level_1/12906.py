c8511ebaab9ef9b71fb782c7d6a72debc95e2c3a
Seohyun Jang <seohyun_j@kakao.com>
2022-04-21 17:15:45 +0900

    같은 숫자는 싫어

--- Programmers/Level_1/12906.py
+++ Programmers/Level_1/12906.py
@@ -4,5 +4,17 @@ arr1 = [4,4,4,3,3]
 def solution(arr):
   return [num for i, num in enumerate(arr) if arr[i]!=arr[i-1] or i==0]
 
+
+# 두번째 내 풀이는 아래와 같은데 이게 효율성 테스트 면에서 훨 빠르다
+def second_solution(arr):
+  answer = []
+  for i in arr:
+    if not answer:
+      answer.append(i)
+    elif i != answer[-1]:
+      answer.append(i)
+  return answer
+
+
 print(solution(arr0))
 print(solution(arr1))
