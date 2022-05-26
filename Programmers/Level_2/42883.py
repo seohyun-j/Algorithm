8ce7d27504556e962e0c6122035e947afaa8cf97
Seohyun Jang <seohyun_j@kakao.com>
2022-05-26 23:23:47 +0900

    큰 수 만들기

--- Programmers/Level_2/42883.py
+++ Programmers/Level_2/42883.py
@@ -15,6 +15,7 @@ def solution(number, k):
 
     return answer
 
+
 # 내가 푼 문제 풀이 방법 -> 정확성 테스트에서 시간초과로 걸림
 from itertools import combinations
 def solution(number, k):
@@ -27,6 +28,14 @@ def solution(number, k):
                 tmp += number[j]
         answer = max(int(tmp), answer)
     return str(answer)
+
+
+# 위의 식 간단화 하면 아래와 같이 되는데 이것도 시간초과
+# 따라서 조합을 사용함에 있어 많은 시간이 발생하므로 조합 사용 X
+def solution(number, k):
+    cnt = list(combinations(list(number), len(number) - k))
+    return ''.join(sorted(cnt, reverse=True)[0])
+
 
 print(solution(n0, k0))
 print(solution(n1, k1))
