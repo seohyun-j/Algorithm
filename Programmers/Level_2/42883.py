02ea4e88e237f32d94a9d3cef7a6b394de4b447c
Seohyun Jang <seohyun_j@kakao.com>
2022-05-26 23:18:46 +0900

    큰 수 만들기

--- Programmers/Level_2/42883.py
+++ Programmers/Level_2/42883.py
@@ -15,6 +15,18 @@ def solution(number, k):
 
     return answer
 
+# 내가 푼 문제 풀이 방법 -> 정확성 테스트에서 시간초과로 걸림
+from itertools import combinations
+def solution(number, k):
+    answer = 0
+    cnt = list(combinations(range(len(number)), k))
+    for i in cnt:
+        tmp = ''
+        for j in range(len(number)):
+            if j not in i:
+                tmp += number[j]
+        answer = max(int(tmp), answer)
+    return str(answer)
 
 print(solution(n0, k0))
 print(solution(n1, k1))
