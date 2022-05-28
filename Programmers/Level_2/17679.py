330f4528550e277e5949005eed2309e2f9e0beb4
Seohyun Jang <seohyun_j@kakao.com>
2022-05-28 14:13:01 +0900

    프렌즈 4블록

--- Programmers/Level_2/17679.py
+++ Programmers/Level_2/17679.py
@@ -4,40 +4,28 @@ board0 = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
 board1 = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
 
 
-def match(m, n, board):
-    result = set()
-
-    for y in range(0, n - 1):
-        for x in range(0, m - 1):
-            if board[y][x] == board[y + 1][x + 1] == board[y][x + 1] == board[y + 1][x]:
-                result |= {(y, x), (y + 1, x), (y, x + 1), (y + 1, x + 1)}
-
-    for i, j in result:
-        board[i][j] = ''
-    print(len(result))
-    board = [list(''.join(i) + ' ' * (m - len(''.join(i)))) for i in board]
-
-    return board, 1
-
-
 def solution(m, n, board):
     answer = 0
     board = list(map(list, zip(*board)))
-    for _ in range(3):
+    while True:
         result = set()
 
         for y in range(0, n - 1):
             for x in range(0, m - 1):
-                if board[y][x] == board[y + 1][x + 1] == board[y][x + 1] == board[y + 1][x]:
-                    result |= {(y, x), (y + 1, x), (y, x + 1), (y + 1, x + 1)}
-        print(result)
+                if board[y][x] != ' ':
+                    if board[y][x] == board[y + 1][x + 1] == board[y][x + 1] == board[y + 1][x]:
+                        result |= {(y, x), (y + 1, x), (y, x + 1), (y + 1, x + 1)}
+        answer += len(result)
+        if not result:
+            break
+
         for i, j in result:
             board[i][j] = ''
 
-        board = [list(''.join(i) + ' ' * (m - len(''.join(i)))) for i in board]
-    return board
+        board = [list(' ' * (m - len(''.join(i))) + ''.join(i)) for i in board]
+
+    return answer
 
 
 print(solution(m0, n0, board0))
-print('-----------------------------------')
 print(solution(m1, n1, board1))
