c2fd18b3a28e8a01519119071b4505c8e735fbcf
Seohyun Jang <seohyun_j@kakao.com>
2022-06-03 18:46:27 +0900

    다음 큰 숫자

--- Programmers/Level_2/12911.py
+++ Programmers/Level_2/12911.py
@@ -3,8 +3,8 @@ n0, n1 = 78, 15
 
 def solution(n):
     answer = n
+    bin_n = bin(n)[2:]
     while True:
-        bin_n = bin(n)[2:]
         answer += 1
         bin_a = bin(answer)[2:]
         if bin_n.count('1') == bin_a.count('1'):
