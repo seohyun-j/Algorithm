df39fd9b8c8b6385be06ce36bf8b3045b0571845
Seohyun Jang <seohyun_j@kakao.com>
2022-06-06 23:37:45 +0900

    절댓값 힙

--- Employment_book/Data_Structure/11286.py
+++ Employment_book/Data_Structure/11286.py
@@ -1,1 +1,11 @@
-ㅐ,[]
+import heapq, sys
+
+input = sys.stdin.readline
+
+hq = []
+for _ in range(int(input())):
+    key = int(input())
+    if key == 0:
+        print(heapq.heappop(hq)[1] if len(hq) else 0)
+    else:
+        heapq.heappush(hq, (abs(key), key))
