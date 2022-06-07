283dfe34bfea6f8b9af78b36cc165116e14b9dab
Seohyun Jang <seohyun_j@kakao.com>
2022-06-07 13:29:13 +0900

    키로거

--- Employment_book/Data_Structure/5397.py
+++ Employment_book/Data_Structure/5397.py
@@ -1,21 +1,17 @@
-import sys
-
-input = sys.stdin.readline
-
 for _ in range(int(input())):
     str = input()
     left = []
     right = []
     for i in str:
-        if i == '>':
-            if right:
-                left.append(right.pop())
+        if i == '-':
+            if left:
+                left.pop()
         elif i == '<':
             if left:
                 right.append(left.pop())
-        elif i == '-':
-            if left:
-                left.pop()
+        elif i == '>':
+            if right:
+                left.append(right.pop())
         else:
             left.append(i)
 
