1a4a19fda1d9ddfaa8960904ad6b909911c29eb8
Seohyun Jang <seohyun_j@kakao.com>
2022-06-16 19:54:15 +0900

    N으로 표현

--- Programmers/Level_3/42895.py
+++ Programmers/Level_3/42895.py
@@ -1,7 +1,26 @@
 def solution(N, number):
-    answer = 0
-    return answer
+    dp = [set() for _ in range(9)]
+
+    for i in range(1, 9):
+        dp[i].add(int(str(N) * i))
+        for j in range(1, i // 2 + 1):
+            print(j, i-j)
+            for first in dp[j]:
+                for second in dp[i - j]:
+                    dp[i] |= {first + second, first - second, second - first, first * second}
+
+                    if first != 0:
+                        dp[i].add(second // first)
+
+                    if second != 0:
+                        dp[i].add(first // second)
+        print(dp[i])
+        if number in dp[i]:
+            return i
+
+    return -1
 
 
 print(solution(5, 12))
 print(solution(2, 11))
+print(solution(5, 31168))
