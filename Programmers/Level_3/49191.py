dc7b2f3c81cd35eb1b807d76d0478c71c3c7a164
Seohyun Jang <seohyun_j@kakao.com>
2022-06-18 17:55:55 +0900

    순위

--- Programmers/Level_3/49191.py
+++ Programmers/Level_3/49191.py
@@ -7,8 +7,8 @@ def solution(n, results):
     lose_dic = defaultdict(set)
 
     for win, lose in results:
-        lose_dic[lose].add(win)
-        win_dic[win].add(lose)
+        win_dic[lose].add(win)
+        lose_dic[win].add(lose)
 
     for i in range(1, n + 1):
         for win in win_dic[i]:
