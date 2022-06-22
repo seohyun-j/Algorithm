ba1ba23c5cf96a505eac1487a40f76cd9a0eee3e
Seohyun Jang <seohyun_j@kakao.com>
2022-06-22 22:52:27 +0900

    셔틀버스

--- Programmers/Level_3/17678.py
+++ Programmers/Level_3/17678.py
@@ -1,6 +1,34 @@
+from collections import deque
+
+
 def solution(n, t, m, timetable):
     answer = ''
-    return answer
+    st_h = 9
+    st_m = 0
+    bus = [(st_h, st_m)]
+    for i in range(n - 1):
+        st_m += t
+        if st_m >= 60:
+            st_m -= 60
+            st_h += 1
+        bus.append((st_h, st_m))
+
+    timetable = [tuple(map(int, i.split(':'))) for i in timetable]
+    timetable.sort(key=lambda x: (x[0], x[1]))
+
+    for i in range(n):
+        cnt = 0
+        for j in timetable:
+            if bus[i][0] >= j[0] and bus[i][1] >= j[1]:
+                cnt += 1
+                lh, lm = timetable.pop(0)
+
+        if i == n - 1:
+            if cnt < m:
+                print(cnt)
+                return str(bus[i][0]) + ':' + str(bus[i][1])
+            else:
+                return str(lh) + ':' + str(lm - 1)
 
 
 arr0 = ["08:00", "08:01", "08:02", "08:03"]
@@ -8,7 +36,7 @@ arr1 = ["09:10", "09:09", "08:00"]
 arr2 = ["09:00", "09:00", "09:00", "09:00"]
 arr3 = ["00:01", "00:01", "00:01", "00:01", "00:01"]
 arr4 = ["23:59"]
-arr5 = ["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59",
+arr5 = ["23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59",
         "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"]
 
 print(solution(1, 1, 5, arr0))
