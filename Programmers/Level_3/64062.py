3b70c37d935711158d1c1419078ecc50dfeb2f83
Seohyun Jang <seohyun_j@kakao.com>
2022-07-06 00:56:00 +0900

    징검다리 건너기

---
+++ Programmers/Level_3/64062.py
@@ -0,0 +1,24 @@
+def solution(stones, k):
+    left = 1
+    right = max(stones)
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+
+        for i in stones:
+            if i - mid <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+                
+            if cnt >= k:
+                break
+
+        if cnt >= k:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
+
+
+print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
