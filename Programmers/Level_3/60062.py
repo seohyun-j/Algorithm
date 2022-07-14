7db8ef19d3178d0b2991c35dbe721a153f928c5c
seohyun <seohyun_j@kakao.com>
2022-07-15 01:52:13 +0900

    외벽점검

--- Programmers/Level_3/60062.py
+++ Programmers/Level_3/60062.py
@@ -1,6 +1,25 @@
+from itertools import permutations
+
+
 def solution(n, weak, dist):
-    answer = 0
-    return answer
+    answer = len(dist) + 1
+    length = len(weak)
+    weak = weak + [w + n for w in weak]
+
+    for st in range(length):
+        for fr in permutations(dist):
+            cnt = 1
+            end_point = weak[st] + fr[cnt - 1]
+            for i in range(st, st+length):
+                if end_point < weak[i]:
+                    cnt += 1
+                    if cnt > len(dist):
+                        break
+                    end_point = weak[i] + fr[cnt - 1]
+            if cnt == 1:
+                return cnt
+            answer = min(answer, cnt)
+    return -1 if answer > len(dist) else answer
 
 
 print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]))
