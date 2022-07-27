f334293d5e51e0ad2fbeba1ad69b4297e42d3c79
seohyun <seohyun_j@kakao.com>
2022-07-27 22:42:49 +0900

    110 옮기기

--- Programmers/Level_3/77886.py
+++ Programmers/Level_3/77886.py
@@ -1,11 +1,32 @@
 def solution(s):
+    def extract(word):
+        cnt, stack = 0, []
+        for i in word:
+            if i == '0' and stack[-2:] == ['1', '1']:
+                stack.pop()
+                stack.pop()
+                cnt += 1
+            else:
+                stack.append(i)
+        return ''.join(stack), cnt
+
+    def rearrange(word):
+        for i in range(-1, -len(word) - 1, -1):
+            pointer = len(word) + (i + 1)
+            if word[i] == '0':
+                return word[:pointer] + '110' + word[pointer:]
+        return '110' + word
+
     answer = []
-    stk = []
-    for i in s:
-        for j in range(len(i)):
-            if i[j] == '0' and i[j-2:j-1] == '11':
-                stk.append('110')
-    print(stk)
+    for val in s:
+        # 110 제거 하기
+        val, count = extract(val)
+
+        # 110 정렬하여 넣기
+        for _ in range(count):
+            val = rearrange(val)
+
+        answer.append(val)
     return answer
 
 
