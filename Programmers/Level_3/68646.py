e6d8c55759980c80e591e62cbd2ec1501cbf06fd
seohyun <seohyun_j@kakao.com>
2022-07-30 01:31:05 +0900

    공 이동 시뮬레이션

--- Programmers/Level_3/68646.py
+++ Programmers/Level_3/68646.py
@@ -1,7 +1,7 @@
 def solution(a):
     result = [False for _ in range(len(a))]
     min_left, min_right = float("inf"), float("inf")
-    
+
     for i in range(len(a)):
         if a[i] < min_left:
             min_left = a[i]
--- Programmers/Level_3/87391.py
+++ Programmers/Level_3/87391.py
@@ -6,35 +6,27 @@ def solution(n, m, x, y, queries):
     for command, dx in queries[::-1]:
         # ← 이동
         if command == 0:
-            if left == 0:
-                right = min(width, right + dx)
-            else:
+            if left != 0:
                 left += dx
-                right = min(width, right + dx)
+            right = min(width, right + dx)
 
         # → 이동
         elif command == 1:
-            if right == width:
-                left = max(0, left - dx)
-            else:
-                left = max(0, left - dx)
+            if right != width:
                 right -= dx
+            left = max(0, left - dx)
 
         # ↑ 이동
         elif command == 2:
-            if top == 0:
-                bottom = min(height, bottom + dx)
-            else:
+            if top != 0:
                 top += dx
-                bottom = min(height, bottom + dx)
+            bottom = min(height, bottom + dx)
 
         # ↓ 이동
         else:
-            if bottom == height:
-                top = max(0, top - dx)
-            else:
-                top = max(0, top - dx)
+            if bottom != height:
                 bottom -= dx
+            top = max(0, top - dx)
 
         if left > right or top > bottom:
             return 0
