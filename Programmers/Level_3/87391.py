d8cd52ab867cfa8bef38250d03dc5384060c16b9
seohyun <seohyun_j@kakao.com>
2022-07-29 19:20:14 +0900

    공 이동 시뮬레이션

---
+++ Programmers/Level_3/87391.py
@@ -0,0 +1,49 @@
+def solution(n, m, x, y, queries):
+    top = bottom = x
+    left = right = y
+    height = n - 1
+    width = m - 1
+
+    for command, dx in queries[::-1]:
+        # ← 이동
+        if command == 0:
+            if left == 0:
+                right = min(width, right + dx)
+            else:
+                left += dx
+                right = min(width, right + dx)
+
+        # → 이동
+        elif command == 1:
+            if right == width:
+                left = max(0, left - dx)
+            else:
+                left = max(0, left - dx)
+                right -= dx
+
+        # ↑ 이동
+        elif command == 2:
+            if top == 0:
+                bottom = min(height, bottom + dx)
+            else:
+                top += dx
+                bottom = min(height, bottom + dx)
+
+        # ↓ 이동
+        else:
+            if bottom == height:
+                top = max(0, top - dx)
+            else:
+                top = max(0, top - dx)
+                bottom -= dx
+
+        if left > right or top > bottom:
+            return 0
+
+    return (bottom - top + 1) * (right - left + 1)
+
+
+q0 = [[2, 1], [0, 1], [1, 1], [0, 1], [2, 1]]
+q1 = [[3, 1], [2, 2], [1, 1], [2, 3], [0, 1], [2, 1]]
+print(solution(2, 2, 0, 0, q0))
+print(solution(2, 5, 0, 1, q1))
