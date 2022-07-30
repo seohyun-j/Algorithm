74549e7611d10fba345df8f6478ef2a2820d3311
seohyun <seohyun_j@kakao.com>
2022-07-31 01:38:24 +0900

    가장 긴 팰린드롬

--- Programmers/Level_3/12904.py
+++ Programmers/Level_3/12904.py
@@ -1,10 +1,14 @@
 def solution(s):
-    answer = 0
+    answer = 1
     length = len(s)
 
     for i in range(length):
         for j in range(i + 2, length + 1):
             chk = s[i:j]
+
+            if len(chk) > length:
+                continue
+
             if chk == chk[::-1]:
                 answer = max(answer, j - i)
 
