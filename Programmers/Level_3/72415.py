45c89f3cbe110c338ba9b9eaea0bc5532df055ba
seohyun_j <seohyun_j@kakao.com>
2022-08-05 23:47:34 +0900

    test

--- Programmers/Level_3/72415.py
+++ Programmers/Level_3/72415.py
@@ -116,20 +116,19 @@ def solution(board, r, c):
         for k1, k2 in i:
             tmp = [(move(maps, xy, k1) + move(maps, k1, k2), k2) for xy in arr] \
                   + [(move(maps, xy, k2) + move(maps, k2, k1), k1) for xy in arr]
-<<<<<<< HEAD
-            print(tmp)
-            maps[k1[0]][k2[1]] = maps[k2[0]][k1[0]] = 0
 
-            cnt += 2 + (mvn := min(tmp)[0])
-            arr = [xy for m, xy in arr if m == mvn]
-        answer = min(answer, cnt) 
-=======
+        #    print(tmp)
+        #    maps[k1[0]][k2[1]] = maps[k2[0]][k1[0]] = 0
+
+        #    cnt += 2 + (mvn := min(tmp)[0])
+        #    arr = [xy for m, xy in arr if m == mvn]
+        # answer = min(answer, cnt)
             maps[k1[0]][k1[1]] = maps[k2[0]][k2[1]] = 0
 
             cnt += 2 + (mvn := min(tmp)[0])
             arr = [xy for m, xy in tmp if m == mvn]
         answer = min(answer, cnt)
->>>>>>> 9157a4e675c52030d25794215c3a240f62dd2e17
+
     return answer
 
 
